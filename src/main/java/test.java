98f01d326810b25773eb613c9f4937cb58bfa52a
Daniel <bgdppeu@gmail.com>
2019-11-03 16:08:14 +0200

    User insert test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,7 +2,10 @@ import hotelicus.entities.Users;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class test {
     public static void test() {
@@ -12,6 +15,19 @@ public class test {
         @SuppressWarnings("unchecked")
         List<Users> employeeList = session.createQuery("FROM " + Users.class.getSimpleName()).list();
 
+
+        Users user = new Users();
+        user.setUsername("Test 1");
+        user.setPassword("Password");
+        user.setPrivileges(0);
+        user.setFirstName("Papa");
+        user.setLastName("Meme");
+        user.setPhoneNumber("+355555555");
+        user.setUserState(1);
+        user.setCreatedOn(new Date());
+
+        session.save(user);
+        session.getTransaction().commit();
 
         for(Users employee : employeeList) {
             System.out.println(employee.getFirstName());
