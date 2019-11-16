b955416d6547964ca3e0962002fab78e7364185d
Daniel <bgdppeu@gmail.com>
2019-11-16 18:38:56 +0200

    Error dialog, move creating new window

--- src/main/java/hotelicus/controllers/LoginController.java
+++ src/main/java/hotelicus/controllers/LoginController.java
@@ -1,16 +1,11 @@
 package hotelicus.controllers;
 
 import hotelicus.window.Dashboard;
+import hotelicus.window.Error;
 import javafx.fxml.FXML;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -40,22 +35,15 @@ public class LoginController {
 
     @FXML
     private void printOutput() throws IOException {
-//        user.setText(user.getText());
-//        password.setText(password.getText());
         System.out.println(user.getText());
         System.out.println(password.getText());
 
         // Verify user input
-        if (user.getText() != null && password.getText() != null) {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("templates/Dashboard.xml"));
-                Parent root = fxmlLoader.load();
-                Stage stage = new Stage();
-                stage.setTitle("Dashboard");
-                stage.setScene(new Scene(root));
-                stage.show();
+        if (!user.getText().equals("") && !password.getText().equals("")) {
+            new Dashboard();
         } else {
             // return error
-            System.out.println("Empty fields");
+            new Error("Failed to login", "There are empty fields!");
         }
     }
 }
---
+++ src/main/java/hotelicus/window/Error.java
@@ -0,0 +1,15 @@
+package hotelicus.window;
+
+import javafx.scene.control.Alert;
+
+import java.io.IOException;
+
+public class Error {
+    public Error(String errorTitle, String errorMessage) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(errorTitle);
+        alert.setHeaderText(null);
+        alert.setContentText(errorMessage);
+        alert.showAndWait();
+    }
+}
